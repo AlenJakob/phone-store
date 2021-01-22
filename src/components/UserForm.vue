d12739d284e43b0e79dda351713152532b980e14
Alen <33994452+AlenJakob@users.noreply.github.com>
2021-01-22 19:58:01 +0100

    form styling

--- src/components/UserForm.vue
+++ src/components/UserForm.vue
@@ -14,58 +14,60 @@
           >
             Order
           </h2>
-
-          <form class="mt-10">
-            <!-- Email Input -->
-            <div v-for="(val, key) in getUserPersonalInfo" :key="key.id">
-              <label
-                for="email"
-                class="block text-xs font-semibold text-gray-600 uppercase"
-                >{{ key }}</label
-              >
-              <input
-                :value="val"
-                :id="val"
-                type="email"
-                name="email"
-                placeholder="e-mail address"
-                autocomplete="email"
-                class="block w-full py-3 px-1 mt-2 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200"
-                required
-              />
-            </div>
-            <div
-              v-for="(val, key) in this.currentUser[0].address"
-              :key="key.id"
-            >
-              <label
-                for="email"
-                class="block text-xs font-semibold text-gray-600 uppercase"
-                >{{ key }}
-              </label>
-              <input
-                :value="val"
-                :id="val"
-                type="email"
-                name="email"
-                placeholder="e-mail address"
-                autocomplete="email"
-                class="block w-full py-3 px-1 mt-2 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200"
-                required
-              />
-            </div>
-            <!-- Password Input -->
-
-            <!-- Auth Buttton -->
-            <button
-              type="submit"
-              class="w-full py-3 mt-10 bg-gray-800 rounded-sm font-medium text-white uppercase focus:outline-none hover:bg-gray-700 hover:shadow-none"
-              @click.prevent="ConfirmOrder"
-            >
-              Confirm Order
-            </button>
-          </form>
         </div>
+      </div>
+    </div>
+    <div class="grid place-items-center mx-2 my-20 sm:my-auto">
+      <div
+        class="w-11/12 p-12 sm:w-8/12 md:w-6/12 lg:w-5/12 2xl:w-4/12 px-6 py-10 sm:px-10 sm:py-6 bg-white rounded-lg shadow-md lg:shadow-lg"
+      >
+        <form class="mt-10">
+          <!-- Email Input -->
+          <div v-for="(val, key) in getUserPersonalInfo" :key="key.id">
+            <label
+              for="email"
+              class="block text-xs font-semibold text-gray-600 uppercase"
+              >{{ key }}</label
+            >
+            <input
+              :value="val"
+              :id="val"
+              type="email"
+              name="email"
+              placeholder="e-mail address"
+              autocomplete="email"
+              class="block w-full py-3 px-1 mt-2 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200"
+              required
+            />
+          </div>
+          <div v-for="(val, key) in this.currentUser[0].address" :key="key.id">
+            <label
+              for="email"
+              class="block text-xs font-semibold text-gray-600 uppercase"
+              >{{ key }}
+            </label>
+            <input
+              :value="val"
+              :id="val"
+              type="email"
+              name="email"
+              placeholder="e-mail address"
+              autocomplete="email"
+              class="block w-full py-3 px-1 mt-2 text-gray-800 appearance-none border-b-2 border-gray-100 focus:text-gray-500 focus:outline-none focus:border-gray-200"
+              required
+            />
+          </div>
+          <!-- Password Input -->
+
+          <!-- Auth Buttton -->
+          <button
+            type="submit"
+            class="w-full py-3 mt-10 bg-gray-800 rounded-sm font-medium text-white uppercase focus:outline-none hover:bg-gray-700 hover:shadow-none"
+            @click.prevent="ConfirmOrder"
+          >
+            Confirm Order
+          </button>
+        </form>
       </div>
     </div>
   </div>
@@ -115,7 +117,7 @@ export default {
       delete objectCopy.address;
       return objectCopy;
     },
-  }
+  },
 };
 </script>
 
